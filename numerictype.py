2e3400638d067bdaa97cb7a955804c9f5ff57bdd
TankNajnin09 <tankashraf771@gmail.com>
2025-07-30 21:16:32 -0700

    Add files via upload

---
+++ numerictype.py
@@ -0,0 +1,31 @@
+
+#integer numeric
+a=10
+print("Value of A = ",a)
+print("Datatype of A = ",type(a))
+
+#float numeric
+b=10.10
+print("Value of B = ",b)
+print("Datatype of B = ",type(b))
+
+#boolean type(bool)
+c=10
+d=20
+if(c<d):
+    print("D is big...")
+else:
+    print("D is small...")
+
+#complex type
+c=(10==10)
+print(c)
+d=(10>10)
+print(d)
+e=(10>=10)
+print(e)
+
+#None type
+a=None
+print(a)
+print(type(a))
